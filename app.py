5427e1e98f4c99e26f535fa6e0ee0ba71e72eea8
GeorgeLuo <root@Georges-Mini.fios-router.home>
2016-12-30 04:37:45 -0500

    pymongo functionality added

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask,render_template,jsonify,json,request,Response
 import boto3
 import boto.ec2
+import pymongo
+from pymongo import MongoClient
 
 application = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client.aws_search
+query_results = db.query_results
 
 ec2 = boto3.client('ec2', region_name='us-west-2')
 regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
+
+def saveQuery(query):
+    query_results.delete_many({})
+    query_results.insert_one(query)
 
 @application.route("/makeQuery",methods=['POST'])
 def makeQuery():
@@ -29,20 +38,29 @@ def makeQuery():
         response_pack = {}
         response_pack['message'] = 'Success: results added to table'
         response_pack['results'] = region_to_instance
+        saveQuery(region_to_instance)
+        region_to_instance.pop('_id')
         return Response(response=json.dumps(response_pack), status='201',mimetype="application/json")
 
     except Exception,e:
+        print (e)
         return jsonify(status='ERROR',message="Error: check credentials")
 
 @application.route('/')
 def genTables():
     return render_template('list.html')
 
-
 @application.route("/getRegions",methods=['POST'])
 def getRegions():
     regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
     return json.dumps(regions)
+
+@application.route("/initializeQuery",methods=['POST'])
+def initializeQuery():
+    print ('generating persisted data...')
+    query = query_results.find_one()
+    query.pop('_id')
+    return jsonify(results=query)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
